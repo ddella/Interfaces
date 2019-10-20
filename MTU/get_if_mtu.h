ff63a21fc7a591613535b641e1102c47b9e34eed
ddella <48737096+ddella@users.noreply.github.com>
2019-10-20 19:48:15 -0400

    Create get_if_mtu.h

---
+++ MTU/get_if_mtu.h
@@ -0,0 +1,10 @@
+#ifndef __GET_IF_MTU_H__
+#define __GET_IF_MTU_H__
+
+	/************************************************/
+	/***          Functions prototype             ***/
+	/************************************************/
+
+int get_if_mtu (uint32_t *, const char *);
+
+#endif
